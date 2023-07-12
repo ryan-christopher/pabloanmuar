4e24852479c8d81fec85a815ba429181f7bbfdac
ryan-christopher <rechristopher99@gmail.com>
2023-07-12 14:46:08 -0400

    landing pic, 3d model added

---
+++ src/components/Guitar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Canvas, useLoader } from '@react-three/fiber'
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { OrbitControls } from "@react-three/drei"
+import { Suspense } from 'react'
+
+const Model = () => {
+    const guitar = useLoader(FBXLoader, "./guitar/guitar.fbx");
+    guitar.position.y = -2.5
+    guitar.position.x = 0.3
+    guitar.rotation.x = 0.25;
+    guitar.rotation.z = 0.14
+    return <primitive object={guitar} scale={0.006} />;
+};
+
+function Guitar() {
+    return (
+        <div>
+            <Canvas frameloop="demand" className='block ml-auto mr-auto !h-[400px] !w-[300px] hover:cursor-pointer'>
+                <ambientLight intensity={1.5} />
+                <Suspense fallback={null}>
+                    <OrbitControls autoRotate={true} autoRotateSpeed={3.0} enableZoom={false} enablePan={false} minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
+                    <Model />
+                </Suspense>
+            </Canvas>
+        </div>
+    )
+}
+
+export default Guitar
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,14 @@
+import React from 'react'
 // Styles
 import "../styles/App.css"
 import "../styles/index.css"
 //import { Link } from "react-router-dom"
 import Navbar from "../components/Navbar"
-
-import React from 'react'
+import Guitar from '../components/Guitar'
+// Moving Text
+import { Parallax } from 'react-scroll-parallax';
+import { ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax';
+import Marquee from "react-fast-marquee";
 
 function home() {
     return (
@@ -24,10 +28,24 @@ function home() {
 
             <div id="home" className="min-h-[100vh] w-[100vw] bg-[#222725]">
 
-                <div className="text-white font-['Bungee_Shade'] absolute bottom-[20px] right-[30px] text-[40px] md:text-[70px]">
-                    <div className="rotate-[-90deg] translate-x-[120px] translate-y-[-100px] md:translate-x-[210px] md:translate-y-[-170px]">Música</div>
-                    <div>Trágame &#x2022;</div>
-                </div>
+                <ParallaxBanner style={{ aspectRatio: '1 / 1' }} className="h-[120vh] max-w-[100vw]">
+                    <ParallaxBannerLayer image="landingpic.jpeg" speed={10} />
+                    <ParallaxBannerLayer className='bg-[#08080861] ease-in duration-200' speed={0}>
+
+                        <div className="text-white font-['Bungee_Shade'] absolute bottom-[23vh] right-[50px] text-[40px] md:text-[70px]">
+                            <div className="rotate-[-90deg] translate-x-[120px] translate-y-[-100px] md:translate-x-[210px] md:translate-y-[-170px]">Música</div>
+                            <div>Trágame &#x2022;</div>
+                        </div>
+
+                        <span className='asteriskcontainer block ml-auto mr-auto w-[100vw] align-center text-center mt-[50px]'>
+                            <Parallax rotate={[0, 360]} className="w-[10vw] min-w-[40px] inline-block max-w-[55px] mt-[30px] 
+                        ease-linear duration-[50ms] mr-[8vw] ml-[8vw]">
+                                !
+                            </Parallax>
+                        </span>
+                    </ParallaxBannerLayer>
+                </ParallaxBanner>
+
 
             </div>
 
@@ -36,7 +54,7 @@ function home() {
             </div>
 
             <div id="about" className="min-h-[100vh] w-[100vw] bg-[#f7ce5b]">
-
+                <Guitar />
             </div>
 
             <div id="contact" className="min-h-[100vh] w-[100vw] bg-[#e9f1f7]">
