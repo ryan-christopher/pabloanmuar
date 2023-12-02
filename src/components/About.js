969df63d6268bd2d5e5a42ff3f52787bc276d380
ryan-christopher <rechristopher99@gmail.com>
2023-12-02 00:11:00 -0500

    edits to featured and about sections

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax';
 import Guitar from '../components/Guitar'
+import "../styles/offsetBorder.css"
 
 function About() {
     return (
@@ -19,8 +20,8 @@ function About() {
                 </ParallaxBanner>
             </div>
             <div className="w-[100vw] bg-[#f7ce5b] pb-[150px]">
-                <div className="w-[90vw] ml-[5vw] pt-[75px]">
-                    <div className='font-["Fredoka"] text-black text-[25px] lg:text-[30px] pt-[50px]'>
+                <div className="font-['Patrick_Hand'] w-[90vw] ml-[5vw] pt-[75px]">
+                    <div className='text-black text-[25px] lg:text-[25px] pt-[0px] w-[50vw] leading-[50px] text-left'>
                         I have always been fascinated by the amount of different styles of music that the human race has evolved to produce.
                         Generations after generations of artistic expression that go all the way back to our ancestors who lived in caverns
                         and worshiped fire. If anything, this shows how music is something bound to our most primitive instincts.
@@ -28,19 +29,18 @@ function About() {
                         beings.
                     </div>
 
-                    <div className='bg-[#d1603d] xl:mt-[10%] mt-[30px] h-[500px] w-[30vw] block ml-auto mr-auto'>
-                        <div>
-                            What I pursue with my music, is to create sounds that feel human; sounds that connect with the most inner fiber of our beings.
-                        </div>
+                    <div className='bg-[#d1603d] offsetBorder !border-black p-[10px] mt-[50px] h-[700px] w-[40vw] rounded-[5px] block ml-auto mr-auto'>
+                        <div className='text-white'>
+                            What I pursue with my music, is to create sounds that feel human; sounds that connect with the most inner fiber of our beings.                         </div>
                         <div className=''>
                             <Guitar />
                         </div>
-                        <div>
+                        <div className='text-white'>
                             I have always seen music as a language that can connect people no matter where they come from, or what language they speak.
                         </div>
                     </div>
 
-                    <div className='font-["Fredoka"] text-black text-[25px] lg:text-[30px] pt-[20px] xl:pt-[500px]'>
+                    <div className='text-black text-[25px] lg:text-[25px] pt-[20px] xl:pt-[50px] w-[50vw] leading-[50px] text-right block ml-auto'>
                         I have always seen music as a language that can connect people no matter where they come from, or what language
                         they speak. In the same way, my music is the reflection of this belief: I want to create music that doesn’t need
                         translation from culture to culture, language to language. How can one achieve this? The answer is creating a sound
